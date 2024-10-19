4c3a4e3f6f94ae888adf81c6e78205286d25d36c
prashanth-cn <prashanth.cn@shopup.org>
2024-10-19 06:30:01 +0530

    fix(build): ci/cd

--- scripts/copyPkgJson.js
+++ scripts/copyPkgJson.js
@@ -6,8 +6,8 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const packageJsonPath = path.resolve(__dirname, '../package.json');
-const packageJsonData = fs.readFileSync(packageJsonPath, 'utf-8');
+const packageJsonPath = path.resolve(__dirname, "../package.json");
+const packageJsonData = fs.readFileSync(packageJsonPath, "utf-8");
 
 // Parse the JSON data into an object
 const packageJson = JSON.parse(packageJsonData);
@@ -15,7 +15,8 @@ const packageJson = JSON.parse(packageJsonData);
 // Read the package.json from the root
 
 // Remove unnecessary fields for the publishable package.json
-const { scripts, devDependencies, ...packageData } = packageJson;
+const { scripts, devDependencies, publishConfig, files, ...packageData } =
+  packageJson;
 
 // Write the new package.json to the dist directory
 fs.writeFileSync(
